cd6150e872556280b0b9c7282273a2109865b63b
Coco <corentin.recanzone@epitech.eu>
2022-10-20 17:28:35 +0200

    m

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -30,6 +30,15 @@
 
       <input id="sendForm" type="submit" value="Login" />
     </form>
+
+    <p class="or-divider">OU CONNECTEZ-VOUS AVEC</p>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px">
+      <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
+      <path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
+      <path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/>
+      <path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/>
+    </svg>
   </main>
 </template>
 
@@ -72,15 +81,27 @@
     input {
       height: 33px;
       background-color: #101117;
+      border: 1px solid #46474b;
       border-radius: 4px;
       color: white;
       width: 100%;
       font-family: 'Poppins-Regular';
+      padding: 10px;
+    }
+
+    input:focus, input:focus:hover {
+      outline: none;
+      border: 1px solid #E0A100;
+    }
+
+    input:hover {
+      outline: none;
+      border: 1px solid #626368;
     }
 
     #sendForm {
       background-color: #E0A100;
-      height: 42px;
+      height: 38px;
       border: 5px;
       width: 90%;
       color: black;
@@ -91,20 +112,50 @@
     #checkboxForm {
       display: flex;
       align-items: center;
-      margin: 10px 0 30px 8px;
+      margin: 10px 0 25px 8px;
       width: 100%;
       font-family: 'Poppins-Regular';
       font-size: 12px;
+      color: #666b6a;
 
       input {
         width: 50px;
-        height: 17px;
+        height: 19px;
         margin-right: -9px;
+        border: 1px solid #626368;
+        border-radius: 6px;
       }
     }
   }
 
   #iconInfos {
     margin-left: 5px;
+  }
+
+  .or-divider{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Poppins-Regular';
+    font-size: 13px;
+    margin: 0 1em;
+    color: #666b6a;
+    margin-top: 25px;
+    font-weight: bold;
+  }
+  .or-divider::before{
+    content: '';
+    height: .125em;
+    background: #666b6a;
+    flex: 1;
+    margin: 0 17px 0 0;
+  }
+  .or-divider::after{
+    content: '';
+    height: .125em;
+    background: #666b6a;
+    flex: 1;
+    margin: 0 0 0 17px;
   }
 </style>
