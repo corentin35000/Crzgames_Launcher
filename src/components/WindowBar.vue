2a5a7b9220f69f5a1e25a47da85efbb43cdaf17e
Coco <corentin.recanzone@epitech.eu>
2022-10-20 22:26:31 +0200

    m

--- src/components/WindowBar.vue
+++ src/components/WindowBar.vue
@@ -1,23 +1,21 @@
 <template>
-  <div>
-    <div data-tauri-drag-region class="titlebar">
-      <div class="titlebar-button" id="titlebar-minimize">
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="#85868a" stroke="#85868a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus">
-          <line x1="5" y1="12" x2="19" y2="12"/>
-        </svg>
-      </div>
+  <div data-tauri-drag-region class="titlebar">
+    <div class="titlebar-button" id="titlebar-minimize">
+      <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="#85868a" stroke="#85868a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus">
+        <line x1="5" y1="12" x2="19" y2="12"/>
+      </svg>
+    </div>
 
-      <!--<div class="titlebar-button" id="titlebar-maximize">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="#85868a" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-square">
-          <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
-        </svg>
-      </div>-->
+    <!--<div class="titlebar-button" id="titlebar-maximize">
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="#85868a" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-square">
+        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
+      </svg>
+    </div>-->
 
-      <div class="titlebar-button" id="titlebar-close">
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="#85868a" stroke="#85868a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
-          <line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/>
-        </svg>
-      </div>
+    <div class="titlebar-button" id="titlebar-close">
+      <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="#85868a" stroke="#85868a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+        <line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/>
+      </svg>
     </div>
   </div>
 </template>
@@ -28,7 +26,7 @@
 
 <style lang="scss" scoped>
   .titlebar {
-    height: 22px;
+    height: 150px;
     user-select: none;
     display: flex;
     justify-content: flex-end;
